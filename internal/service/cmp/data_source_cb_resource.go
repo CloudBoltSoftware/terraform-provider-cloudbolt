6b70486eb65e11817286b0441ec0633bc0f3f6b6
laltomar <laltomar10@gmail.com>
2024-01-22 14:25:35 -0500

    [CMP-1414] - Added Data Sources For CloudBolt Resource and Server

---
+++ internal/service/cmp/data_source_cb_resource.go
@@ -0,0 +1,87 @@
+package cmp
+
+import (
+	"context"
+
+	"github.com/cloudboltsoftware/cloudbolt-go-sdk/cbclient"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func DataSourceCloudBoltResource() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceCloudBoltResourceRead,
+
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The global id of a CloudBolt Resource, required if \"name\" not provided",
+			},
+			"url_path": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The relative API URL path for the CloudBolt Resource.",
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The name of a CloudBolt Resource, required if \"id\" not provided",
+			},
+			"create_date": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Date the was created in CloudBolt",
+			},
+			"status": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "CloudBolt Resource Status",
+			},
+			"attributes": {
+				Type: schema.TypeMap,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Computed:    true,
+				Description: "CloudBolt Resource attributes",
+			},
+		},
+	}
+}
+
+func dataSourceCloudBoltResourceRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	apiClient := m.(*cbclient.CloudBoltClient)
+	urlPath := d.Get("url_path").(string)
+	id := d.Get("id").(string)
+
+	if id == "" && urlPath == "" {
+		return diag.Errorf("Either id or url_path is required")
+	}
+
+	var resource *cbclient.CloudBoltResource
+	var err error
+	if urlPath != "" {
+		resource, err = apiClient.GetResource(urlPath)
+	} else {
+		resource, err = apiClient.GetResourceById(id)
+	}
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if resource.ID == "" {
+		return diag.Errorf("Resource not found.")
+	}
+
+	d.SetId(resource.ID)
+	d.Set("url_path", resource.Links.Self.Href)
+	d.Set("name", resource.Name)
+	d.Set("create_date", resource.Created)
+	d.Set("status", resource.Status)
+	resAttributes, _ := parseAttributes(resource.Attributes)
+	d.Set("attributes", resAttributes)
+
+	return nil
+}
---
+++ internal/service/cmp/data_source_cb_server.go
@@ -0,0 +1,238 @@
+package cmp
+
+import (
+	"context"
+
+	"github.com/cloudboltsoftware/cloudbolt-go-sdk/cbclient"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func DataSourceCloudBoltServer() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceCloudBoltServerRead,
+
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The global id of a CloudBolt Server, required if \"url_path\" not provided",
+			},
+			"url_path": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The relative API URL path for the CloudBolt Server, required if \"id\" not provided",
+			},
+			"hostname": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Server Hostname",
+			},
+			"ip_address": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Server IP Address",
+			},
+			"status": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "CloudBolt Server Status",
+			},
+			"mac": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Server MAC Address",
+			},
+			"power_status": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Server Power Status",
+			},
+			"date_added_to_cloudbolt": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Date the server was added to CloudBolt",
+			},
+			"cpu_count": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "CPU Count",
+			},
+			"memory_size_gb": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Total Memory in GB",
+			},
+			"disk_size_gb": {
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Total Disk Size in GB",
+			},
+			"notes": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Server Notes",
+			},
+			"labels": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Description: "Server Labels",
+			},
+			"os_family": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Server OS Family",
+			},
+			"attributes": {
+				Type:     schema.TypeMap,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Description: "CloudBolt Server attributes",
+			},
+			"rate_breakdown": {
+				Type:     schema.TypeMap,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Description: "Server Rate Breakdown",
+			},
+			"tech_specific_attributes": {
+				Type:     schema.TypeMap,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Description: "Resource Handler technical specific attributes",
+			},
+			"disks": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"uuid": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Unique ID of Disk",
+						},
+						"name": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Name of Disk",
+						},
+						"disk_size_gb": {
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Disk Size in GB",
+						},
+					},
+				},
+				Description: "Server disks",
+			},
+			"networks": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeMap,
+					Elem: &schema.Schema{
+						Type: schema.TypeString,
+					},
+				},
+				Description: "Server NICs",
+			},
+		},
+	}
+}
+
+func dataSourceCloudBoltServerRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	apiClient := m.(*cbclient.CloudBoltClient)
+	serverPath := d.Get("url_path").(string)
+	id := d.Get("id").(string)
+
+	if id == "" && serverPath == "" {
+		return diag.Errorf("Either name or id is required")
+	}
+	var server *cbclient.CloudBoltServer
+	var err error
+	if serverPath != "" {
+		server, err = apiClient.GetServer(serverPath)
+	} else {
+		server, err = apiClient.GetServerById(id)
+	}
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if server.ID == "" {
+		return diag.Errorf("Server not found.")
+	}
+
+	d.SetId(server.ID)
+	d.Set("url_path", server.Links.Self)
+	d.Set("hostname", server.Hostname)
+	d.Set("ip_address", server.IP)
+	d.Set("status", server.Status)
+	d.Set("mac", server.Mac)
+	d.Set("date_added_to_cloudbolt", server.DateAddedToCloudbolt)
+	d.Set("cpu_count", server.CPUCount)
+	d.Set("memory_size_gb", server.MemorySizeGB)
+	d.Set("disk_size_gb", server.DiskSizeGB)
+
+	if server.PowerStatus != "" {
+		d.Set("power_status", server.PowerStatus)
+	}
+
+	if server.Notes != "" {
+		d.Set("notes", server.Notes)
+	}
+
+	if server.Labels != nil {
+		d.Set("labels", server.Labels)
+	}
+
+	if server.OsFamily != "" {
+		d.Set("os_family", server.OsFamily)
+	}
+
+	if server.RateBreakdown != nil {
+		d.Set("rate_breakdown", server.RateBreakdown)
+	}
+
+	if len(server.Disks) > 0 {
+		disks := make([]map[string]interface{}, 0)
+
+		for _, d := range server.Disks {
+			uuid, _ := d["uuid"]
+			name, _ := d["name"]
+			disk_size_gb, _ := d["diskSize"]
+
+			disk := map[string]interface{}{
+				"uuid":         uuid,
+				"name":         name,
+				"disk_size_gb": disk_size_gb,
+			}
+			disks = append(disks, disk)
+		}
+
+		d.Set("disks", disks)
+	}
+
+	if len(server.Networks) > 0 {
+		d.Set("networks", server.Networks)
+	}
+
+	if len(server.TechSpecificAttributes) > 0 {
+		d.Set("tech_specific_attributes", convertValuesToString(server.TechSpecificAttributes))
+	}
+
+	svrAttributes, _ := parseAttributes(server.Attributes)
+	d.Set("attributes", svrAttributes)
+
+	return nil
+}
